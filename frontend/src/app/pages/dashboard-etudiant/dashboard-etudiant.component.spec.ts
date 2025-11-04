2aa1c4106aad0f992033eeb7e7ee1e875b6accbf
sinda bennja <bennjasinda@gmail.com>
2025-11-04 22:10:48 +0100

    Update dashboard-etudiant.component.spec.ts

--- frontend/src/app/pages/dashboard-etudiant/dashboard-etudiant.component.spec.ts
+++ frontend/src/app/pages/dashboard-etudiant/dashboard-etudiant.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardEtudiantComponent } from './dashboard-etudiant.component';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 describe('DashboardEtudiantComponent', () => {
   let component: DashboardEtudiantComponent;
   let fixture: ComponentFixture<DashboardEtudiantComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DashboardEtudiantComponent]
+      declarations: [DashboardEtudiantComponent],
+      imports: [HttpClientTestingModule] 
     });
     fixture = TestBed.createComponent(DashboardEtudiantComponent);
     component = fixture.componentInstance;
